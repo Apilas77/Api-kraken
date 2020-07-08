2334b5d1eefbcf0a6327ee2acbe999dbf73e29c2
henri <m.henrisy@gmail.com>
2020-07-08 12:24:37 +0200

    Auto stash before merge of "master" and "origin/master"

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import requests, json
 from jinja2 import Template
 
 
@@ -7,8 +8,17 @@ with open("index.html.jinja2") as file_:
     template = Template(file_.read())
 
 @app.route("/")
+def hello():  
+    r = requests.get('https://hubeau.eaufrance.fr/api/v1/temperature/station?code_departement=45&pretty')
+    return r.json()
+    #print(r.json)
+    #print(r.headers)
+    #print(r.headers['Content-Type'])
 def hello():
     return template.render()
 
+
 if __name__ == "__main__":
     app.run(debug=True)
+
+
