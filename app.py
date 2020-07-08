87765236269e54ca3886f169cd3495588ab89c50
jojo <thevalkyrie43@gmail.com>
2020-07-08 17:39:49 +0200

    display data Json

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
-from flask import Flask
-import requests, json
-from jinja2 import Template
+import requests
+
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
-with open("index.html.jinja2") as file_:
-    template = Template(file_.read())
 
 @app.route("/")
 def index():
-    r = requests.get('https://hubeau.eaufrance.fr/api/v1/temperature/station?code_departement=45&pretty')
-    return template.render(r.json())
+    url = 'https://hubeau.eaufrance.fr/api/v1/temperature/station?code_departement=45&pretty'
+    try:
+        response = requests.get(url)
+    except requests.ConnectionError:
+        return "Connection Error"
+    data = response.json()
+
+    return render_template("index.html.jinja2", data=data)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
