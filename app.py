19e36b0a109062cdeba299907579ec7a7b6203f3
henri <m.henrisy@gmail.com>
2020-07-08 17:38:57 +0200

    Add CSS

--- app.py
+++ app.py
@@ -1,17 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
 import requests, json
-from jinja2 import Template
+#from jinja2 import Template
 
 
 app = Flask(__name__)
-with open("index.html.jinja2") as file_:
-    template = Template(file_.read())
+#with open("index.html") as file_:
+    #template = Template(file_.read())
 
 @app.route("/")
 def index():
     r = requests.get('https://hubeau.eaufrance.fr/api/v1/temperature/station?code_departement=45&pretty')
-    return template.render(r.json())
+    return render_template("index.html")
 
+@app.route("/Acceuil")
+def acceuil():
+    return render_template("Acceuil.html")
+
+@app.route("/Actualités")
+def Actualités():
+    return render_template("Actualités.html")
+
+@app.route("/Contact")
+def Contact():
+    return render_template("Contact.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
