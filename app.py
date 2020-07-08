d0d3f0986f632c3f871d942c729eeaed1a5ce0dd
jojo <thevalkyrie43@gmail.com>
2020-07-08 11:22:24 +0200

    Add jinja template
    we need to use a template engine for our API

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from jinja2 import Template
+
 
 app = Flask(__name__)
+with open("index.html.jinja2") as file_:
+    template = Template(file_.read())
 
 @app.route("/")
 def hello():
-    return "bonjour les enfants testA"
+    return template.render()
 
 if __name__ == "__main__":
     app.run(debug=True)
