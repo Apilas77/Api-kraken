a6d9a6ac77f6782c26c44ed0cf6206133795df8c
jojo <thevalkyrie43@gmail.com>
2020-07-08 12:30:25 +0200

    set up resquest on the API

--- app.py
+++ app.py
@@ -8,14 +8,9 @@ with open("index.html.jinja2") as file_:
     template = Template(file_.read())
 
 @app.route("/")
-def hello():  
+def index():
     r = requests.get('https://hubeau.eaufrance.fr/api/v1/temperature/station?code_departement=45&pretty')
-    return r.json()
-    #print(r.json)
-    #print(r.headers)
-    #print(r.headers['Content-Type'])
-def hello():
-    return template.render()
+    return template.render(r.json())
 
 
 if __name__ == "__main__":
