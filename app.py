cbce5bc0b3ee9bbd4a65e4ce993a3b8d1bbee426
jojo <thevalkyrie43@gmail.com>
2020-07-10 13:21:44 +0200

    update

--- app.py
+++ app.py
@@ -10,16 +10,18 @@ def index():
     resultats = ""
     departements = ""
     station = ""
+    station_pro = ""
 
     code_region = request.form.get("code_region")
     code_departement = request.form.get("code_departement")
     code_station = request.form.get("code_station")
+    code_station_pro = request.form.get("code_station_pro")
 
     url_region = "https://geo.api.gouv.fr/regions/"
     url_departement = f"https://geo.api.gouv.fr/regions/{code_region}/departements"
     url_finale = f'https://hubeau.eaufrance.fr/api/v1/temperature/station?code_departement={code_departement}&pretty'
     url_station = f"https://hubeau.eaufrance.fr/api/v1/temperature/chronique?code_station={code_station}&size=5&sort=desc&pretty"
-
+    url_station_pro = f"https://hubeau.eaufrance.fr/api/v1/temperature/chronique?code_station={code_station_pro}&size=5&sort=desc&pretty"
     try:
         response = requests.get(url_region)
     except requests.ConnectionError:
@@ -43,6 +45,12 @@ def index():
         except requests.ConnectionError:
             return "Connection Error results"
         station = station.json()
+    if request.method == "POST" and code_station_pro:
+        try:
+            station = requests.get(url_station_pro)
+        except requests.ConnectionError:
+            return "Connection Error results"
+        station_pro = station.json()
 
     return render_template(
         "index.html",
@@ -51,6 +59,7 @@ def index():
         code_region=code_region,
         code_departement=code_departement,
         code_station=code_station,
+        code_station_pro=station_pro,
         region=data_region,
         resultats=resultats,
         test=url_station
